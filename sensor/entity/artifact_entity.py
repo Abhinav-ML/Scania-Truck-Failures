f560f346055bc673d97c13a1a70b447c6ffc9195
Abhinav Kumar <abhinav.ai.ml@gmail.com>
2023-03-25 16:28:18 +0530

    added model_pusher.py

--- sensor/entity/artifact_entity.py
+++ sensor/entity/artifact_entity.py
@@ -10,6 +10,7 @@ class DataIngestionArtifact:
 class DataValidationArtifact:
     report_file_path:str
 
+
 @dataclass
 class DataTransformationArtifact:
     transform_object_path:str
@@ -22,7 +23,13 @@ class ModelTrainerArtifact:
     model_path:str 
     f1_train_score:float 
     f1_test_score:float
+
 @dataclass
 class ModelEvaluationArtifact:
     is_model_accepted:bool
     improved_accuracy:float
+
+@dataclass
+class ModelPusherArtifact:
+    pusher_model_dir:str 
+    saved_model_dir:str
