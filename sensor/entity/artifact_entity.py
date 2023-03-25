dc5714ba77b04bbb279a2f78a7af6a197ffe9269
Abhinav Kumar <abhinav.ai.ml@gmail.com>
2023-03-25 15:41:54 +0530

    added model_trainer.py

--- sensor/entity/artifact_entity.py
+++ sensor/entity/artifact_entity.py
@@ -15,4 +15,10 @@ class DataTransformationArtifact:
     transform_object_path:str
     transformed_train_path:str
     transformed_test_path:str
-    target_encoder_path:
+    target_encoder_path:str
+
+@dataclass
+class ModelTrainerArtifact:
+    model_path:str 
+    f1_train_score:float 
+    f1_test_score:float
